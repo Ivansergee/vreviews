f73012141ebd3e41c5746c4c6047558da05d38c7
Ivan Klimentev <klimentievis@inbox.ru>
2022-04-06 07:45:31 +0300

    start backend

--- reviews_django/reviews_django/settings.py
+++ reviews_django/reviews_django/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -27,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    # 'rest_framework_authtoken',
+    'rest_framework.authtoken',
     'corsheaders',
     'djoser',
 
